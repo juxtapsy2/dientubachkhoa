8cbce82039cea4a3ce244c741747ceab75b5d29b
juxtapsy2 <frost.death.ap@gmail.com>
2025-07-02 13:16:13 +0700

    Initial

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeroSection() {
+    return (
+      <section className="bg-blue-100 p-8 text-center">
+        <h1 className="text-3xl font-bold">Trung Tâm Điện Tử Bách Khoa</h1>
+        <p>Fast & affordable repairs for all TV brands.</p>
+      </section>
+    );
+  }
+  
