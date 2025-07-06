d87bc08b7632098e99e80cecfd79db1aa3379489
juxtapsy2 <frost.death.ap@gmail.com>
2025-07-07 04:28:20 +0700

    Fix sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://suatvbachkhoa.com',
+    siteUrl: 'https://suatvbachkhoa.com.vn',
     generateRobotsTxt: true, // will generate /robots.txt and include sitemap automatically
 };
   
